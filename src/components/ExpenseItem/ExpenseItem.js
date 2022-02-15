057c9556e4856218916ea52f04b4b38f90c7a5de
Akshat42 <akshatdivya03@gmail.com>
2022-02-15 12:18:58 +0530

    Add ExpenseDate component

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,9 +1,13 @@
+import ExpenseDate from "../ExpenseDate/ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = ({ date, name, amount }) => {
+    const month = date.toLocaleDateString("en-US", { month: "long" });
+    const year = date.getFullYear();
+    const day = date.getDate();
     return (
         <div className="expense-item">
-            <div>{date.toISOString()}</div>
+            <ExpenseDate month={month} year={year} day={day}></ExpenseDate>
             <div className="expense-item__description">
                 <h2>{name}</h2>
                 <div className="expense-item__price">{amount}</div>
